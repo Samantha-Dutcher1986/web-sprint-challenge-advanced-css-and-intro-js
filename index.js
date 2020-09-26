d76dd175a219ecc6c4a4914d7c0fb5acc2a7bf53
Samantha-Dutcher1986 <68967583+Samantha-Dutcher1986@users.noreply.github.com>
2020-09-26 13:59:09 -0500

    Task 4 keeps giving me a syntax error and I'm not sure why

--- index.js
+++ index.js
@@ -207,12 +207,17 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+function changeName(array, index, name) {
+  array[index].name = name;
+  return array;
+}
+console.log(changeName(artists, 9, "Vincent Van Gogh"));
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -221,21 +226,40 @@ const artists = [
  * 
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
+ * 
 */
+// function getArtistByIndex(array, index) {
+//   //console.log(array[index]);
+//   return `The artist at index ${index} is ${array[index].name}`;
+// }
+// console.log(getArtistByIndex(artists, 9));
+
 function getArtistByIndex(array, index) {
-    /* code here */
+    return(artists, [2]);
   }
+  console.log(`The artist at index ${2} is ${artists[2].name}.`);
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
+// function get20s(array){
+//   const years = 
+//   for(let i = 0, years > i, i++);
 
-  /* Code here */
+// };
+function get20s(years) {
+  const birthDeath = years.filter(years >= 1900 && years <= 2000);
+  if (birthDeath === years.filter(years >= 1900 && years <= 2000){
+    return array;
+  }
+};
+console.log(get20s(birthDeath));
 
-}
+
+// const results = years.filter(years >= 1900 && <= 2000);
+// console.log(results);
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -248,10 +272,12 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(array, index) {
+    array.splice(index, 1);
+    return array;
   }
-  
+  console.log(removeArtist(artists, 3));
+
  
 
 /**
@@ -267,11 +293,19 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
-
-    /* Code here */
-
-  }
+function addArtist(object) {
+  artists.push(object);
+  return artists;
+}
+console.log(
+  addArtist({
+    name: "Samantha Dutcher",
+    years: "1986 - 2020",
+    genre: "Web Design",
+    nationality: "Italian",
+    bio: "Single mom on the come-up in life!",
+  })
+);
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -281,11 +315,17 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
-
-  /* Code here */
-
+function lotsOfArt(array) {
+  let newArtist = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i].paintings > 100) {
+      newArtist.push(array[i].name);
+    }
+  }
+  //console.log(newArtist);
+  return newArtist;
 }
+console.log(lotsOfArt(artists));
 
 
 
@@ -326,6 +366,15 @@ function randomize(/* Code here */){
     /* Code here */
 
   }
+
+  function randomize(array) {
+    array.sort(() => Math.random() - 0.75);
+    return array;
+  }
+  let numbers = [1, 2, 3, 4, 5];
+  randomize(numbers);
+  //alert(numbers);
+  console.log(randomize(numbers));
 
 
  /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
